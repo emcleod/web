81298e13f75cf0e69af6ce66de7b485e30529e49
Elaine McLeod <em.mcleod@gmail.com>
2024-09-23 12:19:02 +0100

    Refactoring the triangle tool

--- jtc/src/tools/shapes/Triangle.js
+++ jtc/src/tools/shapes/Triangle.js
@@ -1,21 +1,100 @@
 import { createBaseTool } from "./BaseTool";
-import { DEFAULT_LINE_TYPE, DEFAULT_LINE_WIDTH } from "./ToolUtils";
+import { DEFAULT_LINE_TYPE, DEFAULT_LINE_WIDTH, ToolType } from "./ToolUtils";
+import { fabric } from "fabric";
 
 let _triangleCounter = 0;
 
 const triangleImplementation = {
   name: "triangle",
   buttonId: "triangle-btn",
+  toolType: ToolType.SHAPE,
   selectedTriangle: null,
   triangle: null,
   startPoint: null,
 
   onStartDrawing: function(canvas, o) {
     this.startPoint = this.getPointer(canvas, o.e);
-    this.triangle = new fabric.Triangle({
+    this.triangle = this._createTriangle(this.startPoint);
+    this.addObject(canvas, this.triangle);
+  },
+
+  onKeepDrawing: function(canvas, o) {
+    if (!this.triangle || !this.startPoint) return;
+    const pointer = this.getPointer(canvas, o.e);
+    this._updateTriangle(this.triangle, this.startPoint, pointer);
+    canvas.renderAll();
+  },
+
+  onFinishDrawing: function(canvas, o) {
+    if (!this.triangle || !this.startPoint) return;
+    this.triangle.objectCaching = true;
+    this.triangle.setCoords();
+    this.selectedTriangle = this.triangle;
+    this.editingTool(canvas, this.triangle);
+    this.triangle = null;
+    this.startPoint = null;
+  },
+
+  onActivate: function (canvas) {},
+
+  onDeactivate: function(canvas) {
+    this.selectedTriangle = null;
+    this.triangle = null;
+    this.startPoint = null;
+  },
+
+  currentValues: function () {
+    return this.selectedTriangle
+      ? {
+          strokeWidth: this.selectedTriangle.strokeWidth,
+          strokeDashArray: this.selectedTriangle.strokeDashArray,
+        }
+      : {
+          strokeWidth: DEFAULT_LINE_WIDTH,
+          strokeDashArray: null,
+        };
+  },
+
+  getToolHTML: function (currentValues) {
+    // No custom HTML for now
+    return '';
+  },
+
+  getAdditionalOptions: function (toolOptions) {
+    // No additional options for now
+    return {};
+  },
+
+  decorate: function (
+    canvas,
+    triangle,
+    lineWidth = DEFAULT_LINE_WIDTH,
+    lineType = DEFAULT_LINE_TYPE,
+    additionalOptions = {}
+  ) {
+    if (!triangle || !canvas) return;
+
+    const strokeDashArray =
+      lineType === 'dotted'
+        ? [1, 1]
+        : lineType === 'dashed'
+        ? [5, 5]
+        : null;
+
+    this.setObjectProperties(triangle, {
+      stroke: DEFAULT_LINE_TYPE,
+      strokeWidth: lineWidth,
+      strokeDashArray: strokeDashArray,
+    });
+
+    canvas.renderAll();
+  },
+
+  _createTriangle: function(startPoint) {
+    return new fabric.Triangle({
       _uid: _triangleCounter++,
-      left: this.startPoint.x,
-      top: this.startPoint.y,
+      left: startPoint.x,
+      top: startPoint.y,
       width: 0,
       height: 0,
       stroke: DEFAULT_LINE_TYPE,
@@ -25,41 +104,18 @@ const triangleImplementation = {
       evented: false,
       objectCaching: false
     });
-    this.addObject(canvas, this.triangle);
-    this.selectedTriangle = this.triangle;
   },
 
-  onKeepDrawing: function(canvas, o) {
-    if (!this.triangle || !this.startPoint) return;
-    const pointer = this.getPointer(canvas, o.e);
-    const width = Math.abs(pointer.x - this.startPoint.x);
-    const height = Math.abs(pointer.y - this.startPoint.y);
-    this.triangle.set({
+  _updateTriangle: function(triangle, startPoint, endPoint) {
+    const width = Math.abs(endPoint.x - startPoint.x);
+    const height = Math.abs(endPoint.y - startPoint.y);
+    triangle.set({
       width: width * 2,
       height: height,
-      left: Math.min(this.startPoint.x, pointer.x),
-      top: Math.min(this.startPoint.y, pointer.y),
+      left: Math.min(startPoint.x, endPoint.x),
+      top: Math.min(startPoint.y, endPoint.y),
     });
-    canvas.renderAll();
   },
-
-  onFinishDrawing: function(canvas, o) {
-    this.triangle.objectCaching = true;
-    this.triangle.setCoords();
-    this.selectedTriangle = this.triangle;
-    this.setActiveObject(canvas, this.triangle);
-    this.triangle = null;
-    this.startPoint = null;
-  },
-
-  onActivate: function (canvas) {
-  },
-
-  onDeactivate: function(canvas) {
-    this.selectedTriangle = null;
-    this.triangle = null;
-    this.startPoint = null;
-  }
 };
 
 export const TriangleTool = createBaseTool(triangleImplementation);
